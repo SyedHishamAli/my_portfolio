41dd7673d7d7b4486cbba521e477863471982682
Syed Hisham Ali <hisham.ali@technoduxx.com>
2025-11-25 19:03:27 +0530

    with '#' will be ignored, and an empty message aborts the commit.

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -14,12 +14,11 @@ const Clients = () => {
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10">
-        <div
-          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
-          className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
-        >
+        {/* remove bg-white dark:bg-black dark:bg-grid-white/[0.05], 
+            h-[40rem] to 30rem , md:h-[30rem] are for the responsive design */}
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
           <InfiniteMovingCards
-            items={testimonials}
+            items={testimonials as any}
             direction="right"
             speed="slow"
           />
