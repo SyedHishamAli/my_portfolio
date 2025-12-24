f2b9cd513bc9de96e177053a6eac2e915deed6d4
SyedHishamAli <165741789+SyedHishamAli@users.noreply.github.com>
2025-12-24 09:44:33 +0530

    Update title in metadata to include full name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "I'm Syed Hisham",
+  title: "I'm Syed Hisham Ali",
   description: "Stunning Modern & Minimal Portfolio Website",
 };
 
